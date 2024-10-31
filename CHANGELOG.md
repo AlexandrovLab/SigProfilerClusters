f7c1f03e3b787cabbd70d9b05e25318d3f90e273
mdbarnesUCSD <mdbarnes@ucsd.edu>
2024-10-31 09:30:54 -0700

    v1.1.3: Support VAF values from more variant callers and add CHANGELOG

---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [1.1.3] - 2024-10-31
+
+### Added
+- Added support for Variant Allele Frequency (VAF) values from the following variant callers:
+  - Sanger
+  - TCGA
+  - standardVC
+  - Mutect2
--- setup.py
+++ setup.py
@@ -3,55 +3,67 @@ import os
 import shutil
 
 
-#remove the dist folder first if exists
+# remove the dist folder first if exists
 if os.path.exists("dist"):
-	shutil.rmtree("dist")
+    shutil.rmtree("dist")
+
 
 def readme():
-	this_directory = os.path.abspath(os.path.dirname(__file__))
-	with open(os.path.join(this_directory, 'README.md'), encoding='latin-1') as f:
-		long_description = f.read()
-		return(long_description)
-	# with open('README.rst') as f:
-	# 	return(f.read())
+    this_directory = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(this_directory, "README.md"), encoding="latin-1") as f:
+        long_description = f.read()
+        return long_description
+    # with open('README.rst') as f:
+    # 	return(f.read())
 
-VERSION = '1.1.2'
 
-def write_version_py(filename='SigProfilerClusters/version.py'):
-	# Copied from numpy setup.py
-	cnt = """
+VERSION = "1.1.3"
+
+
+def write_version_py(filename="SigProfilerClusters/version.py"):
+    # Copied from numpy setup.py
+    cnt = """
 # THIS FILE IS GENERATED FROM SIGPROFILECLUSTERS SETUP.PY
 short_version = '%(version)s'
 version = '%(version)s'
 	
 	"""
-	fh = open(filename, 'w')
-	fh.write(cnt % {'version': VERSION,})
-	fh.close()
+    fh = open(filename, "w")
+    fh.write(
+        cnt
+        % {
+            "version": VERSION,
+        }
+    )
+    fh.close()
+
 
 write_version_py()
 
-setup(name='SigProfilerClusters',
-		version=VERSION,
-		description='SigProfilerClusters tool',
-		long_description= readme(),
-		long_description_content_type='text/markdown',
-		url='',
-		author='Erik Bergstrom',
-		author_email='ebergstr@eng.ucsd.edu',
-		license='UCSD',
-		packages=find_packages(),#['SigProfilerMatrixGenerator'],
-		python_requires=">=3.8",
-		install_requires =[
-			"matplotlib>=3.5",
-			"sigProfilerPlotting>=1.3.18",
-			"SigProfilerMatrixGenerator>=1.2.19",
-			"SigProfilerSimulator>=1.1.5",
-			"statsmodels>=0.9.0",
-			"scipy>=1.1.0",
-			"pandas>=0.23.4",
-			"numpy>= 1.17.3",
-			"SigProfilerExtractor>=1.0.17", 
-			"seaborn>=0.13.0"],
-		include_package_data=True,
-		zip_safe=False)
+setup(
+    name="SigProfilerClusters",
+    version=VERSION,
+    description="SigProfilerClusters tool",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="",
+    author="Erik Bergstrom",
+    author_email="ebergstr@eng.ucsd.edu",
+    license="UCSD",
+    packages=find_packages(),  # ['SigProfilerMatrixGenerator'],
+    python_requires=">=3.8",
+    install_requires=[
+        "matplotlib>=3.5",
+        "sigProfilerPlotting>=1.3.18",
+        "SigProfilerMatrixGenerator>=1.2.19",
+        "SigProfilerSimulator>=1.1.5",
+        "statsmodels>=0.9.0",
+        "scipy>=1.1.0",
+        "pandas>=0.23.4",
+        "numpy>= 1.17.3",
+        "SigProfilerExtractor>=1.0.17",
+        "seaborn>=0.13.0",
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
